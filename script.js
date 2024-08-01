ded85e30193a3c49946ddd9ef148dc4b59bfcbf3
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-08-01 13:25:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -158,3 +158,111 @@ function initGame() {
             fireBullets();
         }
     }
+
+    function createAsteroid() {
+        var type = random(1, 4),
+            coordsX,
+            coordsY;
+
+        switch (type) {
+            case 1:
+                coordsX = random(0, cW);
+                coordsY = 0 - 150;
+                break;
+            case 2:
+                coordsX = cW + 150;
+                coordsY = random(0, cH);
+                break;
+            case 3:
+                coordsX = random(0, cW);
+                coordsY = cH + 150;
+                break;
+            case 4:
+                coordsX = 0 - 150;
+                coordsY = random(0, cH);
+                break;
+        }
+
+        var asteroid = {
+            x: 278,
+            y: 0,
+            state: 0,
+            stateX: 0,
+            width: 134,
+            height: 123,
+            realX: coordsX,
+            realY: coordsY,
+            moveY: 0,
+            coordsX: coordsX,
+            coordsY: coordsY,
+            size: random(1, 3),
+            deg: Math.atan2(coordsX - (cW / 2), -(coordsY - (cH / 2))),
+            destroyed: false
+        };
+        asteroids.push(asteroid);
+    }
+
+    function drawAsteroids() {
+        var distance;
+        for (var i = 0; i < asteroids.length; i++) {
+            if (!asteroids[i].destroyed) {
+                ctx.save();
+                ctx.translate(asteroids[i].coordsX, asteroids[i].coordsY);
+                ctx.rotate(asteroids[i].deg);
+                ctx.drawImage(sprite, asteroids[i].x, asteroids[i].y, asteroids[i].width, asteroids[i].height, -(asteroids[i].width / asteroids[i].size) / 2, asteroids[i].moveY += 1 / (asteroids[i].size), asteroids[i].width / asteroids[i].size, asteroids[i].height / asteroids[i].size);
+                ctx.restore();
+
+                asteroids[i].realX = (0) - (asteroids[i].moveY + ((asteroids[i].height / asteroids[i].size) / 2)) * Math.sin(asteroids[i].deg);
+                asteroids[i].realY = (0) + (asteroids[i].moveY + ((asteroids[i].height / asteroids[i].size) / 2)) * Math.cos(asteroids[i].deg);
+                asteroids[i].realX += asteroids[i].coordsX;
+                asteroids[i].realY += asteroids[i].coordsY;
+
+                distance = Math.sqrt(Math.pow(asteroids[i].realX - cW / 2, 2) + Math.pow(asteroids[i].realY - cH / 2, 2));
+                if (distance < (((asteroids[i].width / asteroids[i].size) / 2) - 4) + 100) {
+                    gameOver = true;
+                    playing = false;
+                    canvas.addEventListener('mousemove', handleAction);
+                }
+            } else if (!asteroids[i].extinct) {
+                explodeAsteroid(asteroids[i]);
+            }
+        }
+
+        if (asteroids.length - destroyed < 10 + (Math.floor(destroyed / 6))) {
+            createAsteroid();
+        }
+    }
+
+    function explodeAsteroid(asteroid) {
+        ctx.save();
+        ctx.translate(asteroid.realX, asteroid.realY);
+        ctx.rotate(asteroid.deg);
+
+        var spriteY,
+            spriteX = 256;
+        if (asteroid.state == 0) {
+            spriteY = 0;
+            spriteX = 0;
+        } else if (asteroid.state < 8) {
+            spriteY = 0;
+        } else if (asteroid.state < 16) {
+            spriteY = 256;
+        } else if (asteroid.state < 24) {
+            spriteY = 512;
+        } else {
+            spriteY = 768;
+        }
+
+        if (asteroid.state == 8 || asteroid.state == 16 || asteroid.state == 24) {
+            asteroid.stateX = 0;
+        }
+
+        ctx.drawImage(spriteExplosion, asteroid.stateX += spriteX, spriteY, 256, 256, -(asteroid.width / asteroid.size) / 2, -(asteroid.height / asteroid.size) / 2, asteroid.width / asteroid.size, asteroid.height / asteroid.size);
+        asteroid.state += 1;
+
+        if (asteroid.state == 31) {
+            asteroid.extinct = true;
+        }
+
+        ctx.restore();
+    }
